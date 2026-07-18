fe85557de77faa23b688c6ddf6581cdc5f194196
Mauricio Vieira dos Santos <mauricio.vieiradossantos@mosolf.de>
2026-07-18 12:05:01 +0000

    XML Annotation Project Created

---
+++ app/products-xml-annotations/webapp/test/integration/pages/JourneyRunner.ts
@@ -0,0 +1,30 @@
+import JourneyRunner from "sap/fe/test/JourneyRunner";
+import ListReport from "sap/fe/test/ListReport";
+import ObjectPage from "sap/fe/test/ObjectPage";
+import CustomProductsSetListGenerated from "./ProductsSetList.gen";
+import CustomProductsSetObjectPageGenerated from "./ProductsSetObjectPage.gen";
+
+const runner = new JourneyRunner({
+    launchUrl: sap.ui.require.toUrl("productsxmlannotations") + "/test/flp.html#app-preview",
+    pages: {
+        onTheProductsSetListGenerated: new ListReport(
+            {
+                appId: "productsxmlannotations",
+                componentId: "ProductsSetList",
+                contextPath: "/ProductsSet"
+            },
+            CustomProductsSetListGenerated
+        ),
+        onTheProductsSetObjectPageGenerated: new ObjectPage(
+            {
+                appId: "productsxmlannotations",
+                componentId: "ProductsSetObjectPage",
+                contextPath: "/ProductsSet"
+            },
+            CustomProductsSetObjectPageGenerated
+        )
+    },
+    async: true
+});
+
+export default runner;
--- app/services.cds
+++ app/services.cds
@@ -1,2 +1,4 @@
 
 using from './products-cap-cds/annotations';
+
+using from './products-xml-annotations/annotations';
